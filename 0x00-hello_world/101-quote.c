b89acf73d9014a6df607ece7fbbfbe0d3b3aa887
Amosjaf <amosaraunah@gmail.com>
2022-12-09 13:12:19 -0800

    Alternative to printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-1'\n");
+	return (1);
+}
