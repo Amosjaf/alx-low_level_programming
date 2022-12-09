70b2ea5b46ffa0c7367db5e0582894c2370fd798
Amosjaf <amosaraunah@gmail.com>
2022-12-08 20:35:19 -0800

    Fix whitespace

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
 /**
  *This is a new printf function
  */
-
 int main(void)
 {
-       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
 	return (0);
 }
