12d1893e674e70e48406db5c9ba7ff3926f587ef
Amosjaf <amosaraunah@gmail.com>
2022-12-09 11:14:28 -0800

    Fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+/*This is a program that utilizes the puts inbuilt function*/
+int main(void)
+{
 /**
  *This is the main function
  */
-int main(void)
-{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
