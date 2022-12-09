889ad0d169d7924ed9299898a194ef5c8271afca
Amosjaf <amosaraunah@gmail.com>
2022-12-09 14:20:03 -0800

    Positive or negative numbers using if else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * 
+ * Description: This is a program that utilizes if else statements to print 
+ * positive and negative random intergers
+ * 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
