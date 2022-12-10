7e7711e3f2bce9b9facebf6157c6aaf8e3c3c019
Amosjaf <amosaraunah@gmail.com>
2022-12-10 11:35:32 -0800

    Three number combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,8 +14,10 @@ int main(void)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				if (i == j || j == k || i == k) continue;
-				else if (i > j || j > k || i > k) continue;
+				if (i == j || j == k || i == k)
+					continue;
+				else if (i > j || j > k || i > k)
+					continue;
 				else {
 					putchar(i);
 					putchar(j);
