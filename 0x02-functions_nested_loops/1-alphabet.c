04a7abbc6da7180f1e68636e8b61ce116bbf4eeb
Amosjaf <amosaraunah@gmail.com>
2022-12-14 21:11:58 +0000

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * Description - C programming basics
  * print_alphabet - prints all letters of the alphabet using putchar
+ * Description - C programming basics
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
