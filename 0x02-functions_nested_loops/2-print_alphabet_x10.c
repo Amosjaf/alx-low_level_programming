d4f3fdad83483415dc7892baf21beef877a2ff6d
Amosjaf <amosaraunah@gmail.com>
2022-12-14 21:52:03 +0000

    Print letters of the alphabet on ten lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all letters of the alphabet 10 times
+ * Description - C programming basics
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
