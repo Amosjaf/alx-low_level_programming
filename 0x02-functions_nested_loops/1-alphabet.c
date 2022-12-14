8afe8b459331c524dc7df59b19fb01e2198c57a5
Amosjaf <amosaraunah@gmail.com>
2022-12-14 18:17:51 +0000

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,6 @@
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
         int i = 'a';
