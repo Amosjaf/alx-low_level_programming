13156dbb25de2e3f0ec2d3f204c002a21e5bf042
Amosjaf <amosaraunah@gmail.com>
2022-12-15 04:56:52 +0000

    Checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,23 +5,10 @@
  * Description - C programming basics
  * Return: Always 0 (Success)
  */
-int _putchar(char c);
 int _islower(int c)
 {
-	int ch = 'a';
-
-	while (ch <= 'z')
-	{
-		if (ch == c)
-		{
-			_putchar('1');
-			break;
-		}
-		else
-		{
-			ch++;
-		}
-	}
-	_putchar('\n');
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
