c3c2e44f034e31fbb6381f78d0d133926c3b2c6a
Amos Araunah <amosaraunah@gmail.com>
2022-12-15 19:39:57 +0100

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,23 @@
-#include "mian.h"
+#include "main.h"
+#include <stdio.h>
 /**
   * _more_numbers - is a function that excludes some numbers
   * Description - uses control statements
   * Return - 0 for success
   */
+void more_numbers(void)
+{
+	int i = '0', j = '0';
 
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	while (j <= '4')
+	{
+		_putchar('1');
+		_putchar(j);
+		j++;
+	}
+}
