2b1263fc9e007112cfca56c3fded96d0e414e3c8
Amosjaf <amosaraunah@gmail.com>
2022-12-15 03:27:39 +0000

    Case conversion

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
