2b4e8eacca19c93f4e885d40220ecb41075e58f7
Amos Araunah <amosaraunah@gmail.com>
2022-12-16 00:23:07 +0100

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * print_diagonal - is a function that prints a diagonal-like pattern
+  * Description - uses putchar
+  * Return - 0 for success
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			putchar(' ');
+		}
+		putchar('\\');
+		putchar('\n');
+	}
+	}
+	else {
+		putchar('\n');
+	}
+}
+int main(void)
+{
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+    return (0);
+}
