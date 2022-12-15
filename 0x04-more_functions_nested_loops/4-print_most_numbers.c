bc6a4fce7bfe06bf79de943a7546a50ebc39bf1b
Amosjaf <amosaraunah@gmail.com>
2022-12-15 15:21:55 +0000

    Numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _print_most_numbers - is a function that excludes some numbers
+* print_most_numbers - is a function that excludes some numbers
 * Description - uses control statements
 * Return - 0 for success
 */
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,7 @@
+#include "mian.h"
+/**
+  * _more_numbers - is a function that excludes some numbers
+  * Description - uses control statements
+  * Return - 0 for success
+  */
+
