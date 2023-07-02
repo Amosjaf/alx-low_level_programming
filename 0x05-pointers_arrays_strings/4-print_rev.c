351d7471b3ed59759222a9193b6cdcc75b80b66b
Amosjaf <amosaraunah@gmail.com>
2023-07-02 22:22:03 +0000

    print reverse of string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void print_rev(char *s);
+
+/**
+ * print_rev - Function that prints reverse of a string
+ * @s: pointer to character string
+ * Description: Prints out individual characters of a string from the
+ * last to the first
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	char *firstStr;
+
+	firstStr = s;
+	while (*s != '\0')
+		s++;
+	while (s != (firstStr - 1))
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
