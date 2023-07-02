133a44fb993966434bca2e81940eae6459bc2e27
Amosjaf <amosaraunah@gmail.com>
2023-07-02 21:42:48 +0000

    Function for print the characters of a string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void _puts(char *str);
+
+/**
+ * _puts - function to write string to standard output
+ * @str: string parameter to be passed to function
+ *
+ * Description: This function accepts a pointer to a string and
+ * writes it to standard output after iterating through individual
+ * characters
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
