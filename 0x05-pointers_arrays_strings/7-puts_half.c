47cd82d556c3a327620b665d66d65000f20001e6
Amosjaf <amosaraunah@gmail.com>
2023-07-04 13:38:53 +0000

    Print distal half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void puts_half(char *str);
+int _putchar(char c);
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to a string being passed as a parameter from main
+ * Description: Finds the length of the string, determines the middle
+ * point then iterates backwards to the middle
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	char *middle;
+	char *end;
+	int count, i, mid;
+
+	middle = str;
+	end = str;
+	count = 0;
+	i = 0;
+	while (*end)
+	{
+		count++;
+		end++;
+	}
+	end--;
+	mid = (count) / 2;
+	middle = str + mid;
+	while (middle <= end)
+	{
+		_putchar(*middle);
+		middle++;
+		i++;
+	}
+	_putchar('\n');
+}
