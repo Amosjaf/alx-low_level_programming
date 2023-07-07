7148e13bdef5d82ae1abecfc675fc50625951021
Amosjaf <amosaraunah@gmail.com>
2023-07-07 12:47:33 +0000

    capitalize all first words preceeded by any character outside A-z

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+char *cap_string(char *);
+
+/**
+ * cap_string - function that capitalizes all the first characters of words
+ * @s: string parameter
+ *
+ * Description: accepts a string as parameter, loops through the characters
+ * while converting only characters preceeded by special characters or
+ * symbols
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+	printf("%c\n", s[0]);
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i - 1] < 'A' || s[i - 1] > 'z')
+				s[i] -= 32;
+		}
+		else
+			continue;
+	}
+	return (s);
+}
