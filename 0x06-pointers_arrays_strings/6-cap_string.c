46c4acc1c0d0ed1bf2718a188ba0db114a96c306
Amosjaf <amosaraunah@gmail.com>
2023-07-07 16:19:53 +0000

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,7 +23,9 @@ char *cap_string(char *s)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			if (s[i - 1] == '-' || s[i - 1] >= '0' && s[i - 1] <= '9')
+			if (s[i - 1] >= '0' && s[i - 1] <= '9')
+				continue;
+			if (s[i - 1] == '-')
 				continue;
 			if (s[i - 1] < 'A' || s[i - 1] > 'z')
 				s[i] -= 32;
