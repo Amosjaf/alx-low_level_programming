c67efdc198a881d7c04c3f5e56de49af99eee56f
Amosjaf <amosaraunah@gmail.com>
2023-07-10 11:47:45 +0000

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ char *_strcat(char *dest, char *src);
 void reverse_array(int *a, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+char *rot13(char *);
