5700bca5bdc49a1980ccc5fadc5c3be170f20f36
Amosjaf <amosaraunah@gmail.com>
2023-08-27 17:50:29 +0000

    _memset function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,9 +4,9 @@
 char *_memset(char *s, char b, unsigned int n);
 /**
 * _memset - Fills the first n bytes of memory pointed to by s
-* @param s: Pointer to the memory area to be filled
-* @param b: Fillvalue character
-* @param n: Number of bytes to fills
+* @s: Pointer to the memory area to be filled
+* @b: Fillvalue character
+* @n: Number of bytes to fills
 * Description: Function works similar to the built in memset function
 * Return: Pointer to the memory area s
 */
